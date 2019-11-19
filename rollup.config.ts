6cb58a44a89a1fe6ad5d5b6364b49388922dd1f5
Daniel Wei <danielwii0326@gmail.com>
2019-11-19 09:23:58 +0800

    fix: load path issue

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,11 +1,13 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable import/no-extraneous-dependencies,@typescript-eslint/no-var-requires */
 import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import camelCase from 'lodash.camelcase';
 import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
 
 const pkg = require('./package.json');
 
@@ -19,12 +21,15 @@ export default {
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['lodash', 'dotenv', 'crypto'],
   watch: {
     include: 'src/**',
   },
   plugins: [
-    builtins(),
+    // Allow json resolution
+    json(),
+    globals(),
+    builtins({ crypto: true }),
     // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
